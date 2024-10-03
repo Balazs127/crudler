636367a90dd44dc237a6daab12cfa4bbdfe85d3d
Balazs Kis <balazskis.work@outlook.com>
2024-10-03 14:40:46 +0100

    Refactor screen component and module list screen

--- scr/components/screens/ModuleViewScreen.js
+++ scr/components/screens/ModuleViewScreen.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text } from 'react-native';
-import Screen from '../layouts/Screen';
+import { StyleSheet, Text } from "react-native";
+import Screen from "../layout/Screen";
 
 export const ModuleViewScreen = () => {
   return (
