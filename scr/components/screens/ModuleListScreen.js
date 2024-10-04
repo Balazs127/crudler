d5ab83fce6d6c12118e4cb4bd5657cd9ed2ea5f4
Balazs Kis <zsomix.balu@gmail.com>
2024-10-04 10:41:39 +0100

    Refactor ModuleListScreen and ModuleAddScreen to add "Add" and "Cancel" buttons, and Icons to add Cancel icon. (#7)

--- scr/components/screens/ModuleListScreen.js
+++ scr/components/screens/ModuleListScreen.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { LogBox, StyleSheet } from "react-native";
 import Screen from "../layout/Screen";
+import Icons from "../UI/Icons.js";
+import { Button, ButtonTray } from "../UI/Button.js";
 import ModuleList from "../entity/modules/ModuleList";
 
 import initialModules from "../../data/modules.js";
@@ -19,18 +21,31 @@ export const ModuleListScreen = ({ navigation }) => {
     setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
   };
 
+  const handleAdd = (module) => setModules([...modules, module]);
+
   const onDelete = (module) => {
     handleDelete(module);
     navigation.goBack();
   };
 
-  const handleSelect = (module) =>
+  const onAdd = (module) => {
+    handleAdd(module);
+    navigation.goBack();
+  };
+
+  const goToViewScreen = (module) =>
     navigation.navigate("ModuleViewScreen", { module, onDelete });
+
+  const goToAddScreen = () => navigation.navigate("ModuleAddScreen", { onAdd });
 
   // View -----------------------------------
   return (
     <Screen>
-      <ModuleList modules={modules} onSelect={handleSelect} />
+      <ButtonTray>
+        <Button icon={<Icons.Add />} label="Add" onClick={goToAddScreen} />
+      </ButtonTray>
+
+      <ModuleList modules={modules} onSelect={goToViewScreen} />
     </Screen>
   );
 };
