a44d11bc946400f185d6db1ed6cb24c30808e8d3
Balazs Kis <zsomix.balu@gmail.com>
2024-10-11 13:50:30 +0100

    Refactor ModuleAddScreen to use controlled inputs for module details (#8)

--- scr/components/screens/ModuleAddScreen.js
+++ scr/components/screens/ModuleAddScreen.js
@@ -1,32 +1,84 @@
-import { StyleSheet, Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import Screen from "../layout/Screen";
 import Icons from "../UI/Icons.js";
 import { Button, ButtonTray } from "../UI/Button.js";
 
 const defaultModule = {
-  ModuleID: Math.floor(100000 + Math.random() * 900000),
-  ModuleCode: "CI6330",
-  ModuleName: "Mobile Application Development",
-  ModuleLevel: 6,
-  ModuleLeaderID: 1,
-  ModuleLeaderName: "Graeme JONES",
-  ModuleImage:
-    "https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg",
+  ModuleID: null, //Math.floor(100000 + Math.random() * 900000),
+  ModuleCode: null, // "CI6330",
+  ModuleName: null, // "Mobile Application Development",
+  ModuleLevel: null, // 6,
+  ModuleLeaderID: null, // 1,
+  ModuleLeaderName: null, // "Graeme JONES",
+  ModuleImage: null, //"https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg",
 };
 
 export const ModuleAddScreen = ({ navigation, route }) => {
   // Initialisations-------------------------
   const { onAdd } = route.params;
+  defaultModule.ModuleID = Math.floor(100000 + Math.random() * 900000);
+  defaultModule.ModuleImage =
+    "https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg";
 
   // State ----------------------------------
+  const [module, setModule] = useState(defaultModule);
+
   // Handlers--------------------------------
-  const handleAdd = () => onAdd(defaultModule);
+  const handleAdd = () => onAdd(module);
   const handleCancel = () => navigation.goBack();
+
+  const handleChange = (field, value) =>
+    setModule({ ...module, [field]: value });
 
   // View -----------------------------------
   return (
     <Screen>
-      <Text>Add</Text>
+      <View style={styles.item}>
+        <Text style={styles.itemLabel}>Module code</Text>
+        <TextInput
+          value={module.ModuleCode}
+          onChangeText={(value) => handleChange("ModuleCode", value)}
+          style={styles.itemTextInput}
+        />
+      </View>
+
+      <View style={styles.item}>
+        <Text style={styles.itemLabel}>Module name</Text>
+        <TextInput
+          value={module.ModuleName}
+          onChangeText={(value) => handleChange("ModuleName", value)}
+          style={styles.itemTextInput}
+        />
+      </View>
+
+      <View style={styles.item}>
+        <Text style={styles.itemLabel}>Module level</Text>
+        <TextInput
+          value={module.ModuleLevel}
+          onChangeText={(value) => handleChange("ModuleLevel", value)}
+          style={styles.itemTextInput}
+        />
+      </View>
+
+      <View style={styles.item}>
+        <Text style={styles.itemLabel}>Module leader</Text>
+        <TextInput
+          value={module.ModuleLeaderName}
+          onChangeText={(value) => handleChange("ModuleLeaderName", value)}
+          style={styles.itemTextInput}
+        />
+      </View>
+
+      <View style={styles.item}>
+        <Text style={styles.itemLabel}>Module image URL</Text>
+        <TextInput
+          value={module.ModuleImage}
+          onChangeText={(value) => handleChange("ModuleImage", value)}
+          style={styles.itemTextInput}
+        />
+      </View>
+
       <ButtonTray>
         <Button icon={<Icons.Add />} label="Add" onClick={handleAdd} />
         <Button icon={<Icons.Cancel />} label="Cancel" onClick={handleCancel} />
@@ -35,6 +87,21 @@ export const ModuleAddScreen = ({ navigation, route }) => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  itemLabel: {
+    color: "grey",
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  itemTextInput: {
+    height: 50,
+    paddingLeft: 10,
+    fontSize: 16,
+    backgroundColor: "white",
+    borderRadius: 7,
+    borderWidth: 1,
+    borderColor: "lightgray",
+  },
+});
 
 export default ModuleAddScreen;
